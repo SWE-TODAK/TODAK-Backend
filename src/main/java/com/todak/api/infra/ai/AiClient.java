f7238ed8e7de824d676c947d9cd4bdbfd72525f7
chlgmltn <c7992933@naver.com>
2025-12-01 23:42:50 +0900

    refact:inputstream 제거

--- src/main/java/com/todak/api/infra/ai/AiClient.java
+++ src/main/java/com/todak/api/infra/ai/AiClient.java
@@ -5,7 +5,6 @@ import com.todak.api.infra.ai.dto.AiSummaryResponseDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.*;
 import org.springframework.stereotype.Component;
 import org.springframework.util.LinkedMultiValueMap;
