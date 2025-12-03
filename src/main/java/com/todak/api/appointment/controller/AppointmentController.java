11991f9372f0b79ead7e7e46ebe2f0e60da21f6d
chlgmltn <c7992933@naver.com>
2025-12-03 22:51:32 +0900

    깃이그노어

--- src/main/java/com/todak/api/appointment/controller/AppointmentController.java
+++ src/main/java/com/todak/api/appointment/controller/AppointmentController.java
@@ -10,7 +10,6 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-//test
 
 @RestController
 @RequiredArgsConstructor
