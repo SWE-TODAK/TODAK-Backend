b9aabb011203b90da8da8e6f57e00bf56712ccef
hong-seo <ghdtj07@gmail.com>
2026-02-24 11:40:09 +0900

    디버깅

--- src/main/java/com/sogong/todak/health/entity/HealthMetric.java
+++ src/main/java/com/sogong/todak/health/entity/HealthMetric.java
@@ -14,6 +14,7 @@ import java.util.UUID;
 public class HealthMetric {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "metric_id")
     private UUID id;
 
     @ManyToOne(fetch = FetchType.LAZY)
