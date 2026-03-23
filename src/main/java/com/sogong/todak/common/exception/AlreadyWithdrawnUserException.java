1d7b84c4ab2a86507960a597e5e78d48e0f62513
chase <chase@chaseui-MacBookAir.local>
2026-03-23 16:27:07 +0900

    feat/회원 탈퇴 기능 추가

--- src/main/java/com/sogong/todak/common/exception/AlreadyWithdrawnUserException.java
+++ src/main/java/com/sogong/todak/common/exception/AlreadyWithdrawnUserException.java
@@ -1,3 +1,4 @@
+//이미 탈퇴했을 경우 예외처리 하기 위해
 package com.sogong.todak.common.exception;
 
 public class AlreadyWithdrawnUserException extends RuntimeException {
--- src/main/java/com/sogong/todak/common/exception/AuthenticationDataCleanupException.java
+++ src/main/java/com/sogong/todak/common/exception/AuthenticationDataCleanupException.java
@@ -1,3 +1,4 @@
+//회원탈퇴 중 토큰/인증 데이터 정리 실패를 감지해서 전체 탈퇴를 막기 위한 내부 서버 예외
 package com.sogong.todak.common.exception;
 
 public class AuthenticationDataCleanupException extends RuntimeException {
