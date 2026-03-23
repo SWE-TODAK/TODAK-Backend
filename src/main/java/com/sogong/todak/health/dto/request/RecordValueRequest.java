30bf475c996514204b1d61ef36abda4bfa21cdc2
CHOI HONGSEO <ghdtj07@gmail.com>
2026-03-23 11:14:07 +0900

    건강지표 api 수정
    * 건강지표 api 수정

--- src/main/java/com/sogong/todak/health/dto/request/RecordValueRequest.java
+++ src/main/java/com/sogong/todak/health/dto/request/RecordValueRequest.java
@@ -9,7 +9,7 @@ public class RecordValueRequest {
     private Double value;
     private Integer systolic;
     private Integer diastolic;
-    private LocalDateTime recordedAt;
+    private LocalDateTime date;
     private Integer beforeMeal;
     private Integer afterMeal;
     private Double totalChol;
