3ef8ed5eb360b443aaa8f125d692d6263474b764
최홍서 <ghdtj07@gmail.com>
2026-03-26 01:06:38 +0900

    db 삭제

--- src/main/resources/db/migration/postgresql/V20260325.1_add_metadata_to_recordings.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE recordings
-    ADD COLUMN hospital_name VARCHAR(255),
-    ADD COLUMN disease_name VARCHAR(255),
-    ADD COLUMN doctor_name VARCHAR(100),
-    ADD COLUMN department_name VARCHAR(100),
-    ADD COLUMN consulted_at TIMESTAMP WITH TIME ZONE;
