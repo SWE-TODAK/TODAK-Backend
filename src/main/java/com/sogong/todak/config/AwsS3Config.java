b14bc399dd305c2131ca3eec972c4e27f7df4676
vsopsw <vsopsw@gmail.com>
2026-03-26 16:41:10 +0900

    Feat/recording file only (#39)
    * stt log
    
    * log 수정
    
    * feat: log 레벨 조정 및 불필요한 로그 삭제
    
    * feat: SttJobWorker 로그 레벨 조정
    
    * logging 수준 변경
    
    * feat: add recording meta data
    
    * 설정 파일 제외하고 sql 파일 rename
    
    * feat: 특정 sql 파일만 업로드
    
    * feat: modify AwsS3Config
    
    * feat: S3Presigner

--- src/main/java/com/sogong/todak/config/AwsS3Config.java
+++ src/main/java/com/sogong/todak/config/AwsS3Config.java
@@ -3,7 +3,7 @@ package com.sogong.todak.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
@@ -14,7 +14,8 @@ public class AwsS3Config {
     public S3Presigner s3Presigner(@Value("${aws.s3.region}") String region) {
         return S3Presigner.builder()
                 .region(Region.of(region))
-                .credentialsProvider(ProfileCredentialsProvider.create("default")) // 명시적으로 default 프로필 지정
+                // 특정 프로필을 찾는 대신, 시스템 환경에 맞는 자격 증명을 자동으로 찾도록 변경
+                .credentialsProvider(DefaultCredentialsProvider.create())
                 .build();
     }
 }
